20da71abd4fb2d2509e74841c30283f1940987d1
Doroteya Nikolova <doroteanikolova93@gmail.com>
2017-03-16 22:47:01 +0200

    solve item-list

--- JavaScript-UI-and-DOM-master/Topics/07. Event Model/homework/tasks/task.js
+++ JavaScript-UI-and-DOM-master/Topics/07. Event Model/homework/tasks/task.js
@@ -1,15 +1,70 @@
 'use strict';
+let input = ['@*@*@*xxx', '1 -1 1 -1 2 1 1 1 1 1 1'];
 
-let d = 4;
 
-function solve(d) {
-    let N = 0;
-    while (d !== 0) {
-        N++;
-        let digits = N.toString().length;
-        d -= temp;
+function solve(args) {
+    const EMPTYFIELD = 0;
+
+    // get postions 
+
+    let getPositionOfSFD = args[0].split(''),
+        getPath = args[1].split(' ').map(Number),
+        collection = { souls: 0, food: 0, deadlock: 0 },
+        kitty = 0,
+        jumpCount = 0,
+        output = '';
+
+    for (let i = 0; i <= getPath.length; i++) {
+
+
+        // check souls
+        if (getPositionOfSFD[kitty] === '@') {
+            collection.souls += 1;
+            getPositionOfSFD[kitty] = EMPTYFIELD;
+        }
+
+        // check foods
+        if (getPositionOfSFD[kitty] === '*') {
+            collection.food += 1;
+            getPositionOfSFD[kitty] = EMPTYFIELD;
+        }
+
+        // check deadlocks
+        if (getPositionOfSFD[kitty] === 'x') {
+            collection.deadlock += 1;
+            getPositionOfSFD[kitty] = EMPTYFIELD;
+
+            // even
+            if (kitty % 2 == 0) {
+                if (collection.souls === 0) {
+                    let output = 'You are deadlocked, you greedy kitty! \n Jumps before deadlock: ' + jumpCount;
+                    return output;
+                } else {
+                    collection.souls -= 1;
+                    getPositionOfSFD[kitty] = EMPTYFIELD;
+                }
+
+                // odd
+            } else {
+                if (collection.food === 0) {
+                    output = 'You are deadlocked, you greedy kitty! \n Jumps before deadlock: ' + jumpCount;
+                    return output;
+                } else {
+                    collection.food -= 1;
+                    getPositionOfSFD[kitty] = EMPTYFIELD;
+                }
+            }
+        }
+        jumpCount += Math.abs(getPath[i]);
+        kitty += getPath[i];
+        if (kitty > getPositionOfSFD.length) {
+            kitty = kitty - getPositionOfSFD.length;
+        } else if (kitty < 0) {
+            kitty = getPositionOfSFD.length + kitty;
+        }
     }
-    return N;
+    output = 'Coder souls collected: ' + collection.souls + '\nFood collected: ' + collection.food + '\nDeadlocks: ' + collection.deadlock;
+    return output
 }
 
-console.log(solve(d));
+console.log(solve(input));
--- JavaScript-UI-and-DOM-master/Workshops/DOM-and-DOM-manupulations/items-control/task/tasks/task-1.js
+++ JavaScript-UI-and-DOM-master/Workshops/DOM-and-DOM-manupulations/items-control/task/tasks/task-1.js
@@ -1,167 +1,136 @@
 /* globals module, document, HTMLElement, console */
+'use strict';
 
 function solve() {
     return function(selector, isCaseSensitive) {
-        var element = selector,
-            fragment,
 
-            addControls,
-            labelAdd,
-            tbAdd,
-            btnAdd,
-
-            searchControls,
-            labelSearch,
-            tbSearch,
-
-            resultControls,
-            listResults,
-            listItemTemplate,
-            btnDeleteListItem,
-            textListItem,
-            listItems;
-
-
-        isCaseSensitive = !!isCaseSensitive;
-        if (typeof element === "string") {
-            element = document.querySelector(element);
-        }
-        if (!element || !(element instanceof HTMLElement)) {
-            throw new Error("Invalid HTML element or selector");
-        }
-
-        fragment = document.createDocumentFragment();
-
-        /* Add Controls: START */
-
-        addControls = document.createElement("div");
-        addControls.className = "add-controls";
-
-        labelAdd = document.createElement("label");
-        labelAdd.innerHTML = "Enter text: ";
-
-        tbAdd = document.createElement("input");
-
-        labelAdd.appendChild(tbAdd);
-
-        btnAdd = document.createElement("a");
-        btnAdd.className = "button";
-        btnAdd.addEventListener("click", onAddButtonClick, false);
-        btnAdd.innerHTML = "Add";
-        btnAdd.style.display = "inline-block";
-
-        addControls.appendChild(labelAdd);
-        addControls.appendChild(btnAdd);
-
-        /* Add Controls: END */
-
-
-        /* Search Controls: START */
-
-        searchControls = document.createElement("div");
-        searchControls.className = "search-controls";
-
-        labelSearch = document.createElement("label");
-        labelSearch.innerHTML = "Search:";
-
-        tbSearch = document.createElement("input");
-
-        tbSearch.addEventListener("input", onSearchTextboxInput, false);
-
-        labelSearch.appendChild(tbSearch);
-
-        searchControls.appendChild(labelSearch);
-
-        /* Search Controls: END */
-
-        /* Result Controls: START */
-
-        resultControls = document.createElement("div");
-        resultControls.className = "result-controls";
-
-        listResults = document.createElement("ul");
-        listResults.className = "items-list";
-
-        listItemTemplate = document.createElement("li");
-        listItemTemplate.className = "list-item";
-
-        btnDeleteListItem = document.createElement("a");
-        btnDeleteListItem.className = "button button-delete";
-        btnDeleteListItem.innerHTML = "X";
-
-        textListItem = document.createElement("strong");
-        listItemTemplate.appendChild(btnDeleteListItem);
-        listItemTemplate.appendChild(textListItem);
-
-        listResults.addEventListener("click", onListResultClick, false);
-
-
-        resultControls.appendChild(listResults);
-
-        listItems = element.getElementsByClassName("list-item");
-
-        /* Result Controls: END */
-
-        fragment.appendChild(addControls);
-        fragment.appendChild(searchControls);
-        fragment.appendChild(resultControls);
-
-        element.appendChild(fragment);
-        element.className += "items-control";
-
-
-        function onAddButtonClick(ev) {
-            var value = tbAdd.value;
-            tbAdd.value = "";
-
-            textListItem.innerHTML = value;
-
-            listResults.appendChild(listItemTemplate.cloneNode(true));
-        }
-
-        function onSearchTextboxInput() {
-            var i = 0,
-                len,
-                text,
-                pattern = tbSearch.value;
-            if (!isCaseSensitive) {
-                pattern = pattern.toLowerCase();
+        // validation 
+        var VALIDATION = {
+                isString: function(str) {
+                    if (typeof str !== 'string') {
+                        throw new Error('Invalid selector!')
+                    }
+                },
+                elementIsExist: function(el) {
+                    if (el === null || el.length === 0) {
+                        throw new Error('Invalid element');
+                    }
+                },
+                validateCaseSensitive: function(x) {
+                    if (typeof(x) !== "boolean") {
+                        throw new Error('Ivalid caseSenstive!')
+                    }
+                }
             }
+            // parameters     
+        var element, addElement, addButton, listItem, item, removeElement, searchElement, input, inputAddTxt, resultControl, allItems, df;
 
-            for (i = 0, len = listItems.length; i < len; i += 1) {
-                text = listItems[i].getElementsByTagName("strong")[0].innerHTML;
+
+        if (isCaseSensitive === undefined) {
+            isCaseSensitive = false;
+        }
+
+        // validation
+        VALIDATION.isString(selector);
+        VALIDATION.validateCaseSensitive(isCaseSensitive);
+
+        element = document.querySelector(selector);
+        df = document.createDocumentFragment();
+
+        VALIDATION.elementIsExist(element);
+
+        ////////////////////// Adding Elements /////////////////////////////////
+        // content
+        addElement = document.createElement('div');
+        addElement.className = 'add-controls';
+        addElement.innerHTML = 'Enter text';
+        df.appendChild(addElement);
+        // input
+        inputAddTxt = document.createElement('input');
+        df.appendChild(inputAddTxt);
+
+        // button to add
+        addButton = document.createElement('button');
+        addButton.className = 'button';
+        addButton.innerHTML = 'Add';
+        df.appendChild(addButton);
+
+        //create list
+        listItem = document.createElement('ul');
+        listItem.className = 'items-list';
+        df.appendChild(listItem);
+
+        // item
+        item = document.createElement('li');
+        item.className = 'list-item';
+
+        // add item on click
+        addButton.addEventListener('click', onClick, false);
+
+        function onClick() {
+            var cloneItem = item.cloneNode(true);
+            cloneItem.innerHTML = inputAddTxt.value;
+            inputAddTxt.value = '';
+            listItem.appendChild(cloneItem);
+        }
+
+
+        ////////////////////// Remove Elements /////////////////////////////////
+        resultControl = document.createElement('div');
+        resultControl.className = 'result-controls';
+
+
+
+        ////////////////////// Search Elements /////////////////////////////////
+
+        // serch
+        searchElement = document.createElement('div');
+        searchElement.className = 'search-controls';
+        searchElement.innerHTML = 'Search';
+        df.appendChild(searchElement);
+
+        // input
+        input = document.createElement('input');
+        input.addEventListener('input', search, false);
+
+        // search
+        function search(pattern) {
+            var value = pattern.target.value;
+            allItems = document.getElementsByClassName('list-item');
+            console.log(allItems);
+
+            for (var i = 0; i < allItems.length; i++) {
+
                 if (!isCaseSensitive) {
-                    text = text.toLowerCase();
-                }
-
-                if (text.indexOf(pattern) < 0) {
-                    listItems[i].style.display = "none";
+                    if (allItems[i].innerHTML.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                        allItems[i].parentElement.style.display = '';
+                    } else {
+                        allItems[i].parentElement.style.display = 'none';
+                    }
                 } else {
-                    listItems[i].style.display = "";
+                    if (allItems[i].innerHTML.indexOf(value) >= 0) {
+                        allItems[i].parentElement.style.display = '';
+                    } else {
+                        allItems[i].parentElement.style.display = 'none';
+                    }
                 }
+                if (value === '') {
+                    allItems[i].parentElement.style.display = '';
+                }
+
             }
         }
+        df.appendChild(input);
 
-        function onListResultClick(ev) {
-            var btn = ev.target,
-                parent;
-            if (btn.className.indexOf("button-delete") < 0) {
-                return;
-            }
 
-            parent = btn;
-            while (parent && parent.className.indexOf("list-item") < 0) {
-                console.log(parent.nodeName);
-                parent = parent.parentNode;
-            }
 
-            if (!parent) {
-                return;
-            }
 
-            listResults.removeChild(parent);
-        }
+        element.appendChild(df);
     };
 }
-
-module.exports = solve;
+// var el = document.createElement('div');
+// el.className = 'root';
+// document.body.appendChild(el);
+// var result = solve();
+// result('#root', false);
+// module.exports = solve;
